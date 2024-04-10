4e701501ceaa7364ff45488ed1c428d20bbf4d1f
KaleyHamilton <kaleyhamilton6@gmail.com>
2024-04-10 17:23:20 -0400

    Database name change

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -5,6 +5,10 @@
   <div class="home">
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
+     <div id="nav">
+      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+    </div>
   </div>
   <div class="footer">
     <Footer />
