64adaa0092bde6c9c203a8fd524e85a37cffd959
KaleyHamilton <kaleyhamilton6@gmail.com>
2024-04-19 11:49:33 -0400

    Added header and footer to about us

--- vue/src/views/AboutUsView.vue
+++ vue/src/views/AboutUsView.vue
@@ -1,11 +1,26 @@
 <template>
+    <div class="header">
+    <Header />
+  </div>
+  <div class="nav-bar">
+    <NavBar />
+  </div>
     <div class="container">
-        <h1>About Us View</h1>
+        <h1>About Us</h1>
     </div>
+   <h3>“A joint a day keeps the bad mood away.” -Unknown</h3>
+    <div class="footer">
+    <Footer />
+  </div>
 </template>
 
 <script>
+import Header from '../components/Header.vue';
+import Footer from '../components/Footer.vue';
+import NavBar from '../components/NavBar.vue';
+
 export default {
+    components: { Header, Footer, NavBar },
     name: "AboutUsView"
 }
 </script>
@@ -13,5 +28,17 @@ export default {
 <style scoped>
 #search-form{
     margin-top: 20px;
+}
+h1 {
+  text-align: center;
+}
+
+h2 {
+  text-align: center;
+}
+
+h3 {
+  text-align: center;
+  font-style: italic;
 }
 </style>
