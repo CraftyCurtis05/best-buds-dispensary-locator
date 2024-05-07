e737e301dd659b8367c383980eae677ac00a5f49
lexusmfannin <lexusmfannin@gmail.com>
2024-05-07 15:58:41 -0400

    tips and tricks and shop updates

--- vue/src/views/ShopView.vue
+++ vue/src/views/ShopView.vue
@@ -1,11 +1,24 @@
 <template>
-    <div class="container">
-        <h1>Shop View</h1>
+  <div class="shop-view">
+    <div class="header-nav">
+        <div class="header"><Header/></div>
+        <div class="nav-bar"><NavBar/></div>
     </div>
+    <div class="shop">
+        <h1>Shop our Favorite Products</h1>
+        <h2>You deserve a little treat</h2>
+    </div>
+    <div class="footer"><Footer/></div>
+  </div>
 </template>
 
 <script>
+import Header from '../components/Header.vue';
+import Footer from '../components/Footer.vue';
+import NavBar from '../components/NavBar.vue';
+
 export default {
+    components: { Header, Footer, NavBar },
     name: "ShopView"
 }
 </script>
@@ -14,4 +27,34 @@ export default {
 #search-form{
     margin-top: 20px;
 }
+
+.shop-view {
+width: 100vw;
+  background: linear-gradient(0deg, rgba(255, 255, 255, 0.497), rgba(255, 255, 255, 0.881)), url('src\\assets\\green_smoke.png');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-attachment: fixed;
+  margin-top: -1vw;
+}
+
+.header-nav {
+    position: fixed;
+}
+
+h1 {
+    text-align: center;
+    font-size: 130%;
+    font-weight: bolder; 
+    padding-top: 15%; 
+}
+
+h2 {
+    text-align: center; 
+    font-style: italic; 
+    font-weight: lighter; 
+    font-size: 100%;
+}
+
+
 </style>
--- vue/src/views/TipsTricksView.vue
+++ vue/src/views/TipsTricksView.vue
@@ -1,12 +1,26 @@
 <template>
     <div class="container">
-        <h1>Tips & Tricks View</h1>
+        <div class="header-nav">
+            <div class="header"><Header/></div>
+            <div class="nav-bar"><NavBar/></div>
+        </div>
+        <div class="tips-and-tricks">
+            <h1>Tips & Tricks</h1>
+            <h2>Something witty about how we have tips and tricks to help you consume your dispensary goodies!</h2>
+            <p>This is where the buttons will go</p>
+        </div>
+       <div class="footer"><Footer/></div>
     </div>
 </template>
 
 <script>
+import Header from '../components/Header.vue';
+import Footer from '../components/Footer.vue';
+import NavBar from '../components/NavBar.vue';
+
 export default {
-    name: "tipstricks"
+    components: { Header, Footer, NavBar },
+    name: "tipsntricks"
 }
 </script>
 
@@ -14,4 +28,39 @@ export default {
 #search-form{
     margin-top: 20px;
 }
+
+.tips-and-tricks-view {
+    width: 100vw;
+    background: linear-gradient(0deg, rgba(255, 255, 255, 0.497), rgba(255, 255, 255, 0.881)), url('src\\assets\\green_smoke.png');
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-attachment: fixed;
+    margin-top: -1vw;
+}
+
+.header-nav {
+    position: fixed;
+}
+
+h1 {
+    text-align: center; 
+    font-size: 130%; 
+    font-weight: bolder; 
+    padding-top: 15%;
+}
+
+h2 { 
+    text-align: center; 
+    font-size: 105%; 
+    font-weight: lighter; 
+}
+
+p {
+    text-align: center; 
+    font-size: 100%; 
+    margin-left: 16%; 
+    margin-right: 16%;
+}
+
 </style>
