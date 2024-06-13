a7cd40e0bb3e3c6043a73fc0e5da1364a7557d7d
CraftyCurtis05 <craftycurtis05@gmail.com>
2024-06-13 12:52:56 -0400

    Too Much Cannabis View progress

--- vue/src/views/TooMuchCannabisView.vue
+++ vue/src/views/TooMuchCannabisView.vue
@@ -1,162 +1,109 @@
 <template>
-    <div class="smoking-cannabis-view">
+    <div class="too-much-view">
         <div class="header-nav">
             <div class="header"><Header/></div>
             <div class="nav-bar"><NavBar/></div>
         </div>  
-        <div class="smoking-tips">
-            <h1> Tips for Smoking Cannabis </h1>
-            <h3> *By following these tips, you can ensure that you are smoking cannabis the right way and getting the most out of your experience. </h3>
+        <div class="symptoms-too-much">
+            <h1> What To Expect If You've Had Too Much Cannabis </h1>
+            <h3> Consuming too much cannabis can lead to a range of symptoms, from anxiety to nausea. </h3>
             <ul class="list">
-                <li> Use a clean pipe or bong. </li>
-                <li> Grind the cannabis finely. </li>
-                <li> Light the bowl evenly. </li>
-                <li> Inhale slowly and deeply. </li>
-                <li> Don't hold the smoke in your lungs for too long. </li>
-                <li> Exhale the smoke slowly. </li>
-                <li> Drink plenty of water. </li>
-                <li> Stay hydrated. </li>
+                <li> Physical Symptoms: </li>
+                <ul class="sub-list">
+                    <li> Dizziness </li>
+                    <li> Dry, red eyes </li>
+                    <li> Fatigue or lethargy </li>
+                    <li> Headaches </li>
+                    <li> Increased heart rate </li>
+                    <li> Nausea and change in appetite </li>
+                    <li> Thirstiness or a dry mouth (aka “cotton mouth”) </li>
+                    <li> Vomiting </li>
+                </ul>
+            </ul>
+            <ul class="list">
+                <li> Psychological Symptoms: </li>
+                <h4> *High doses of cannabis can also cause psychological symptoms like: </h4>
+                <ul class="sub-list">
+                    <li> Anxiety </li>
+                    <li> Confusion </li>
+                    <li> Difficulty concentrating </li>
+                    <li> Mood changes </li>
+                    <li> Thoughts racing </li>
+                </ul>
+                <h4> *In rare cases, it can cause more serious psychological symptoms like: </h4>
+                <ul class="sub-list">
+                    <li> Hallucinations </li>
+                    <li> Panic attacks </li>
+                    <li> Paranoia </li>
+                </ul>
+                <h4> *In some extremely rare cases, cannabis use may contribute to the onset of psychosis. which can include symptoms like: </h4>
+                <ul class="sub-list">
+                    <li> Delusions </li>
+                    <li> Confusion </li>
+                    <li> Memory loss </li>
+                    <li> Disorganized speech </li>
+                    <li> Difficulty feeling or expressing emotions </li>
+                </ul>
+                <h4> If you think you or someone else is experiencing psychosis, contact emergency services or get immediate medical care. </h4>
             </ul>
         </div>
-        <div class="smoker-tips">
-            <h1> Tips for Cannabis Smokers </h1>
-            <h4> If you've found yourself asking, "What is the first feeling of weed?", it's important to understand that each person's reaction 
-                can be quite diverse. As you ponder, "Is it okay to start smoking weed?", remember that preparation is paramount. A comprehensive 
-                understanding of the varying cannabis strains, a cautious starting approach, and ensuring a comfortable, secure environment can lay 
-                the groundwork for an enjoyable and memorable first-time experience. 
-            </h4>
-            <h2> Understanding Cannabis: </h2>
-                <ul class="list">
-                    <li> Cannabis is a versatile plant with hundreds of strains, each one uniquely characterized by its distinct effects. </li>
-                    <li> These strains range from those known for inducing relaxation and tranquility to those that stimulate creativity and energy. </li>
-                    <li> Being aware of these differences is the first step towards selecting a strain that aligns with your expectations and comfort level. </li>
-                    <li> Please check out our <router-link v-bind:to="{ name:'strainguide'}"> Strain Guide </router-link> for information. </li>
-                </ul>
-            <h2> How to Enjoy Your First High: </h2>
-            <h4> *To fully appreciate your first cannabis experience, consider these tips for first-time weed smokers: </h4>
-                <ul class="list">
-                    <li> Choose the Right Strain: </li>
-                        <ul class="sub-list">
-                            <li> Start with a strain known for its mild and relaxing effects. </li>
-                            <li> This can ease you into the world of cannabis, mitigating the potential for any discomfort. </li>
-                            <li> Make sure to check out the strain’s potency and pick the dosage accordingly. </li>
-                            <li> Always start small - you can get more anytime but if you overdo it, you will ruin your first experience. </li>
-                        </ul>
-                    <li> Start Slowly: </li>  
-                        <ul class="sub-list">
-                            <li> Moderation is key when starting your cannabis journey. </li>
-                            <li> Begin with a small quantity and assess your comfort level and reaction. </li>
-                            <li> Remember, you can always smoke more if desired, but reversing the effects of overconsumption can be challenging. </li>
-                        </ul>
-                    <li> Environment Matters: </li>
-                        <ul class="sub-list">
-                            <li> The setting in which you consume cannabis can significantly influence your experience. </li>
-                            <li> Choose a calm, comfortable, and safe environment where you can relax and enjoy the experience without stress or anxiety. </li>
-                        </ul>  
-                    <li> Have a Sober Friend with You: </li>  
-                        <ul class="sub-list">
-                            <li> For your first time, it's advantageous to have a sober, experienced friend by your side. </li>
-                            <li> This person can provide reassurance, guidance, and answer any questions you may have. </li>
-                        </ul> 
-                    <li> Stay Hydrated: </li>
-                        <ul class="sub-list">
-                            <li> Ensure you drink plenty of water before, during, and after smoking. </li>
-                            <li> Hydration can mitigate some of the dry-mouth effects that can come with smoking cannabis. </li>
-                        </ul> 
-                    <li> Eat Before You Smoke: </li>  
-                        <ul class="sub-list">
-                            <li> Cannabis is known to stimulate the appetite. </li>
-                            <li> Having a meal before smoking can prevent overeating or choosing unhealthy snacks when the "munchies" kick in. </li>
-                        </ul> 
-                    <li> Listen to Your Body: </li>   
-                        <ul class="sub-list">
-                            <li> Respect your body's signals. </li>    
-                            <li> If at any point you feel uncomfortable or anxious, stop and take a break. </li>
-                            <li> Your comfort and safety should always be the priority. </li>
-                        </ul>
-                    <li> Stay calm: </li>
-                        <ul class="sub-list">
-                            <li> Here are five things you can do to avoid freaking out on pot: </li>
-                                <ul class="sub-sub-list">
-                                    <li> Fight back with cannabidiol (CBD) - an anxiety-fighting compound that comes handy when it comes to fighting pot jitters. </li>
-                                    <li> Fight back with black pepper. Both pot and pepper affect the body’s endocannabinoid system. The phytocannabinoid-terpenoid effect not only kills anxiety, but it also helps with addiction, depression, as well as pain. </li>
-                                    <li> Don’t panic! You can’t overdose (OD) and die on weed. What hospital Emergency Rooms (ERs) are reporting is increased panic attacks and anxiety reactions, not overdoses. No one is suffering systemic organ failures, or losing their lives because they ingested too much pot. </li>
-                                    <li> Be smart on dosing. When starting out, limit yourself to only 10 milligrams of Tetrahydrocannabinol (THC) and note how it affects you. If smoking it, you’ll know within a few minutes how high you’re going to get. If using edibles, wait at least an hour. </li>
-                                </ul>
-                        </ul>    
-                    <h4> *Cannabis comes in a variety of forms including edibles and concentrates but they are not the best choice for your first time. 
-                        Extracts including vape cartridges are too potent and hard to handle for a novice and edibles are not that predictable in terms 
-                        of dosage and effects (which depend on numerous factors like tolerance and the time of onset) the buzz may start hours from 
-                        ingestion and last for up to 8 hours. 
-                    </h4>            
-                </ul>
-            <h2> Conclusion: </h2> 
-                <ul class="list">
-                    <li> Your first cannabis experience can be a wonderful journey when approached with care and responsibility. </li>
-                    <li> As for the question, "Is it better to smoke weed the first time alone or in company?", having a trusted, sober friend nearby can make the experience more comforting and enjoyable. </li>
-                    <li> By following the tips shared in this guide, first-time cannabis smokers can look forward to a positive and memorable first high. </li>
-                    <li> If you are contemplating smoking cannabis for the first time, undertake comprehensive research and connect with experienced cannabis users for their insights. </li>
-                    <li> This can empower you to make an informed decision about whether cannabis is the right choice for you. </li>
-                    <li> Remember, responsible consumption is crucial for any enjoyable cannabis experience. </li>
-                </ul>   
-        </div>
-        <div class="common-questions">
-            <h1> Common Questions for Smokers </h1>
+        <div class="coping-tips">
+            <h1> Coping Tips: </h1>
+            <h3> *Consuming too much cannabis is rarely a medical emergency, despite how uncomfortable you may feel. 
+                Symptoms will subside as the effects wear off. In the meantime, these tips can help things feel more manageable: </h3>   
             <ul class="list">
-                <li> What is the method of smoking cannabis? </li>
-                    <ul class="sub-list">
-                        <li> The most common method of smoking cannabis is to use a pipe, bong, or joint. </li>
-                        <li> A pipe is a small, handheld device with a bowl at one end and a mouthpiece at the other. </li>
-                        <li> A bong is a larger device with a water chamber that cools the smoke before it is inhaled. </li>
-                        <li> A joint is a rolled cigarette made of cannabis and tobacco. </li>
-                    </ul>
-                <li> How do you take a good hit? </li>
-                    <ul class="sub-list">
-                        <li> To take a good hit, first grind the cannabis. </li>
-                        <li> Then, pack the bowl of your pipe or bong with the ground cannabis. </li>
-                        <li> Light the bowl and inhale slowly and deeply. </li>
-                        <li> Hold the smoke in your lungs for just a few seconds before exhaling. </li>
-                    </ul>
-                <li> How can I get higher smoking? </li>
-                    <ul class="sub-list">
-                        <li> There are a few things you can do to get higher when smoking cannabis. </li>
-                        <li> First, use more potent strains. </li>
-                        <li> Second, take bigger hits. </li>
-                        <li> And third - do not hold in the smoke. </li>
-                        <li> The THC is absorbed in the lungs almost instantaneously so holding the smoke will lead to absorbing stuff you do not need and can actually be harmful to your lungs. </li>
-                    </ul>
-                <li> How to smoke cannabis the first time? </li>
-                    <ul class="sub-list">
-                        <li> If you are smoking cannabis for the first time, it is important to start slowly and gradually increase the amount you smoke. </li>
-                        <li> This will help you avoid getting too high and feeling sick. </li>
-                        <li> It is also important to be in a comfortable and relaxed setting when you first smoke cannabis. </li>
-                        <li> Having a friend who knows cannabis help you is a great option. </li>
-                    </ul>
-                <li> How to inhale smoke for the first time? </li>
-                <h4> *If you are new to smoking, it may be difficult to inhale the smoke properly. Here are a few tips: </h4>
-                    <ul class="sub-list">
-                        <li> Start by taking small, shallow breaths. </li>
-                        <li> Don't try to inhale too much smoke at once. </li>
-                        <li> Hold the smoke in your lungs briefly before exhaling. </li>
-                        <li> Practice inhaling and exhaling with a clean pipe or bong before you smoke cannabis. </li>
-                    </ul>    
-                <li> Does coughing get you higher? </li>
-                    <ul class="sub-list">
-                        <li> Coughing does not get you higher. </li>
-                        <li> In fact, it can actually make you feel less high. </li>
-                        <li> When you cough, you are expelling the smoke from your lungs before it has a chance to be absorbed into your bloodstream. </li>
-                    </ul>
-                <li> Does breathing deeper get you higher? </li>
-                    <ul class="sub-list">
-                        <li> Breathing deeper will not get you higher, but it can help you to absorb the smoke more efficiently. </li>
-                        <li>  When you breathe deeply, you are taking in more oxygen, which helps to carry the THC (the active ingredient in cannabis) into your bloodstream. </li>
-                    </ul>
-                <li> What is better for first time cannabis use? </li>
-                    <ul class="sub-list">
-                        <li> Please check out our <router-link v-bind:to="{ name:'strainguide'}"> Strain Guide </router-link> for information. </li>
-                    </ul>
+                <li> Remember that it will pass </li>
+                <ul class="sub-list">
+                    <li> When you have a bad reaction to cannabis, you may begin to worry that something is severely wrong with you or that it may not end. </li>
+                    <li> Try to remind yourself that these feelings are temporary and will likely disappear within a few hours. </li>
+                </ul>
+                <li> Sleep it off </li>
+                <ul class="sub-list">
+                    <li> If you’re experiencing physical or psychological symptoms, sleep might be the best thing for you. </li>
+                    <li> You’ll probably feel better when you wake up. Prepare to feel a bit tired and groggy, though. </li>
+                    <li> Keep a glass of water handy, and place a bucket next to your bed if you’re feeling nauseous. </li>
+                </ul>
+                <li> Stay hydrated </li>
+                <ul class="sub-list">
+                    <li> Sip on water. </li>
+                    <li> This is especially helpful if you’re experiencing cotton mouth, nausea, or headaches. </li>
+                    <li> The repetitive sensation of sipping water may also help you feel a little calmer. </li>
+                    <li> If you’ve been throwing up, an electrolyte drink might be helpful. </li>
+                </ul>
+                <li> Relax </li>
+                <ul class="sub-list">
+                    <li> Overstimulation and stress can make you feel more on edge. </li>
+                    <li> If you’re in a crowd or a noisy area, try to go somewhere quiet where you can relax and unwind — even if it’s just a bathroom or toilet stall. </li>
+                    <li> Alternatively, getting some fresh air on a balcony or in a garden might help soothe anxiety, nausea, and racing thoughts. </li>
+                </ul>
+                <li> Use heat </li>
+                <ul class="sub-list">
+                    <li> If you’re experiencing the symptoms of CHS, such as nausea, vomiting, and abdominal pain, heat might be helpful. </li>
+                    <li> Although there are currently no formal treatments for CHS, a 2022 review suggested that some people with CHS can frequently take hot baths and showers. </li>
+                    <li> This might bring them temporary relief, as can applying capsaicin cream to their stomachs. </li>
+                    <li> A hot water bottle may also help. </li>
+                    <li> Why heat? It seems that a peripheral tissue receptor interacts with the endocannabinoid system. </li>
+                    <li> It is also the only known capsaicin receptor, and heat affects it. </li>
+                </ul>
             </ul>
         </div>
+        <div class="what-to-do-too-much">
+            <h1> What To Do If You've Had Too Much Cannabis </h1>
+            <h3> *While these symptoms usually don’t cause lasting health effects, there are a few cases when you may want to get medical care. </h3>
+            <ul class="list">
+                <li> Counteract effects with CBD </li>
+                <ul class="sub-list">
+                    <li> *PUT SOMETHING HERE!!!* </li>
+                </ul>
+            </ul> 
+            <ul class="list">
+                <li> Chew on a whole black peppercorn </li>
+                <ul class="sub-list">
+                    <li> *PUT SOMETHING HERE!!!* </li>
+                </ul>
+            </ul> 
+        </div>
+        
         <div class="want-more">
             <h1> Do You Want More Information? </h1>
             <div id="articles-link">
