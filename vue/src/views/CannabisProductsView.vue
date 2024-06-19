8e4d6cbe9b6a81be4f28c0a634589f1509ab5048
KaleyHamilton <kaleyhamilton6@gmail.com>
2024-06-19 18:25:48 -0400

    Cannabis Products Updates

--- vue/src/views/CannabisProductsView.vue
+++ vue/src/views/CannabisProductsView.vue
@@ -11,29 +11,46 @@
                 <h2>Edibles</h2>
                 <p>Edibles are food products infused with cannabis extract. Edibles come in many forms, including baked goods, candies, gummies, chocolates, lozenges, and beverages.</p>
                 <h3>Pros</h3>
-                <li></li>
-                <li></li>
+                <li>Better for patients with respiratory and cardiovascular conditions, or for individuals who want to avoid smoking</li>
+                <li>Come in an infinite array of options, so consumers can take edibles that appeal to their taste buds</li>
+                <li>Can be bought or homemade</li>
+                <li>Last longer than other methods, which can offer prolonged relief from symptoms of medical conditions such as chronic pain</li>
+                <li>A more discreet means of consumption</li>
+                <li> According to a 2016 study, edibles have been anecdotally reported to assist with symptoms of conditions like muscle spasms, pain, nausea and vomiting, cancer, appetite stimulation, and epilepsy, as well as several psychiatric disorders such as depression and PTSD.</li>
                 <h3>Cons</h3>
-                <li></li>
-                <li></li>
+                <li>Effects and benefits take longer to kick in</li>
+                <li>Easy to consume too much, whether by miscalculating dosages or not waiting long enough before taking more</li>
+                <li>Though rare, there is a risk of inducing psychosis when consuming too much.</li>
+                <li> Potential side effects associated with overconsumption, such as confusion, anxiety, panic, paranoia, hallucinations, delusions, increased blood pressure, faster heart rate, and nausea</li>
+                <li>According to the same 2016 study, overconsumption of edibles has been linked to: cognitive and motor impairment, extreme sedation, agitation, anxiety, cardiac stress, and vomiting.</li>
             </ul>
             <ul id="labels-start-small">
                 <h2>Flower</h2>
                 <p>Cannabis smoking is the inhalation of smoke or vapor released by heating the flowers, leaves, or extracts of cannabis and releasing the main psychoactive chemical, Δ9-tetrahydrocannabinol (THC), which is absorbed into the bloodstream via the lungs. Cannabis can be smoked in a variety of pipe-like implements made in different shapes and of different materials including hand pipes ("bowls"), water pipes ("bongs"), cigarettes ("joints"), or blunts.</p>
                 <h3>Pros</h3>
-                <li></li>
-                <li></li>
+                <li>Rapid onset rate, which means that consumers will feel effects and benefit from cannabis almost immediately</li>
+                <li>Easier to manage how much is being consumed</li>
+                <li>Delivers higher bioavailability rates in comparison to other methods</li>
+                <li>Effects fade more quickly (about 1-2 hours); overdoing it is more difficult and will wear off sooner and less intensely than with edibles</li>
+                <li>Has a reduced risk of being eaten by pets and children since it does not look like a delicious treat</li>
+                <li>Quick and convenient means of consumption</li>
                 <h3>Cons</h3>
-                <li></li>
-                <li></li>
+                <li>May compromise respiratory function since some of the same chemicals found in cigarettes can be found in cannabis smoke</li>
+                <li>Has a shorter duration in terms of experiencing effects and benefiting from compounds</li>
+                <li>Compounds can be lost in side-stream smoke</li>
+                <li>Similarly to edibles, and other means of consumption, smoking cannabis with a potency that is too high can lead to adverse effects such as anxiety.</li>
+                <li>Emits a noticeable smell</li>
                 
             </ul>
             <ul id="too-much-edibles">
                 <h2>Oil</h2>
                 <p>Hash oil or cannabis oil is an oleoresin obtained by the extraction of cannabis or hashish. It is a cannabis concentrate containing many of its resins and terpenes - in particular, tetrahydrocannabinol, cannabidiol, and other cannabinoids. Hash oil is usually consumed by smoking, vaporizing or eating.</p>
                 <h3>Pros</h3>
-                <li></li>
-                <li></li>
+                <li>Cannabis oil is one of the most commonly prescribed home treatments for inflammation, chronic pain, and sometimes even acute pain.</li>
+                <li>Cannabis oil contains a comound called cannabinoids, including THC. This compound attaches itself to brain cells that are responsible for controlling excitable nerves and inducing relaxation. In this way, it helps epilepsy patients control the intensity of their seizures, although it cannot treat them completely.</li>
+                <li>THC, a cannabinoid oil compound, slows the buildup of amyloid plaque. It does so by inhibiting the enzyme that makes this plaque. Amyloid plaque kills brain cells, which ultimately leads to Alzheimer’s disease. Although there is no known cure for this disease, cannabis oil is definitely helpful in maintaining healthy brain cell count for a longer period of time.</li>
+                <li>Cannabis oil can help in improving the health of the cardiovascular system by balancing out the unhealthy oils in the human body. In addition, this oil can scrape off excess cholesterol from the heart and the arteries. In addition, cannabis oil also stimulate antioxidant processes that keep the system healthy, by minimizing the buildup of free radicals.</li>
+                <li>Studies suggest that using cannabis oil can ease the pain symptoms of multiple sclerosis. The THC in this oil attaches itself to nerve receptors in order to relieve pain. There are other studies that suggest that THC can control the spasming of muscles.</li>
                 <h3>Cons</h3>   
                 <li></li>
             </ul>
