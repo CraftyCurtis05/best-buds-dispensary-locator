a1d3b149ec3b54b96888200ad444f5096fb9c1c8
CraftyCurtis05 <craftycurtis05@gmail.com>
2024-07-25 14:57:29 -0400

    Added extra parameter to URL call for more specific search results

--- vue/src/services/YelpService.js
+++ vue/src/services/YelpService.js
@@ -9,7 +9,7 @@ export default{
 
   //Get green function using location Id that connects to Yelp API using bearer token for authorization
   getGreen(locationID) {
-    let queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location="+locationID+"&term=dispensary&radius=20000&sort_by=distance&limit=20";
+    let queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location="+locationID+"&term=dispensary+cannabis&radius=40000&sort_by=distance";
     const apiKey = "_VJj6fsEV44UAy-Cr65nlJl3GVdKOgK-vjlwwC7Nd6mxMJdt3rVRPGrJ8ZerjT-M7NseO0eyVDRSl2ZrOn2Ck6A8BZmdOUX0svKN-3cQfKt_dtgBaPpLk-RNfoGiZnYx";
     
     return axios.get(queryURL, {
