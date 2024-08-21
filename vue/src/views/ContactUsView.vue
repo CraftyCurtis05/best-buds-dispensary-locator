fae13b00c7467163f2d47fb19909c3b2a3504d0e
KaleyHamilton <kaleyhamilton6@gmail.com>
2024-08-21 18:35:02 -0400

    Contact form updates

--- vue/src/views/ContactUsView.vue
+++ vue/src/views/ContactUsView.vue
@@ -26,9 +26,59 @@
 
             <!-- Display Contact Us Form -->
             <section id="contact-us-form">
-                <form>
-                    ***NEED CONTACT US FORM***
-                </form>
+                <div class="contact-container" style="width: 450px;">
+    <card class="card-section" style="width: 450px; margin: auto">
+      <form ref="values" @submit.prevent="sendEmail">
+        <div class="form-group">
+          <input
+            class="form-input"
+            name="name"
+            v-model="user_name"
+            placeholder="Name"
+            required
+          />
+        </div>
+       
+        <div class="form-group">
+          <input
+            class="form-input"
+            name="email"
+            v-model="user_email"
+            placeholder="Email"
+            required
+          />
+        </div>
+
+        <div class="form-group">
+            <input
+                class="form-input"
+                name="favstrain"
+                v-model="fav_strain"
+                placeholder="Favorite Strain"
+                required
+            />
+        </div>
+
+        <div class="form-group">
+          <textarea
+            class="form-input"
+            name="message"
+            v-model="user_message"
+            placeholder="Message"
+            :rows="4"
+            required
+          />
+        </div>
+
+        <div class="example-col">
+          <Button
+            id="submit-btn"
+            >Submit</Button
+          >
+        </div>
+      </form>
+    </card>
+  </div>
             </section>
 
         </main>      
@@ -51,10 +101,45 @@
 import Header from '@/components/Header.vue';
 import QuoteComponent from '@/components/Quote.vue';
 import Footer from '@/components/Footer.vue';
+// import emailjs from 'emailjs-com';
 
 export default {
     name: "ContactUs",
-    components: { Header, QuoteComponent, Footer }
+    components: { Header, QuoteComponent, Footer },
+    data() {
+    return {
+      user_name: "",
+      user_email: "",
+      fav_strain: "",
+      user_message: "",
+    };
+  },
+  methods: {
+    // sendEmail() {
+    //   emailjs
+    //     .sendForm(
+    //       "service_bmvzxb3",
+    //       "template_jrn3z3o",
+    //       this.$refs.values,
+    //       "xKi3BvVcjev9tM0vx"
+    //     )
+    //     .then(
+    //       (result) => {
+    //         console.log(
+    //           "You have successfully submitted your message",
+    //           result.text
+    //         );
+    //          location.reload(true);
+    //       },
+    //       (error) => {
+    //         console.log(
+    //           "This form failed to submit, please kindly check your internet connection",
+    //           error.text
+    //         );
+    //       }
+    //     );
+    // },
+  },
 };
 </script>
 
@@ -93,6 +178,134 @@ h2 {
 
 p {
     margin: auto 8vw;
+}
+
+.form-input{
+  margin: 10px;
+  padding: 2%;
+  padding-left: 3%;
+}
+.form-group{
+  display: flex;
+  justify-content: center;
+}
+.form-group input{
+  font-size: 100%;
+  font-family: Poppins;
+}
+.form-group textarea{
+  font-size: 100%;
+  font-family: Poppins;
+} 
+
+#submit-btn {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: bold;
+  font-size: 100%;
+  height: 50px;
+  width: 100px;
+  margin: auto;
+  padding: 0.6em 2em;
+  border: none;
+  outline: none;
+  color: rgb(255, 255, 255);
+  background: #111;
+  cursor: pointer;
+  position: relative;
+  z-index: 0;
+  border-radius: 10px;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+}
+
+#submit-btn:before {
+  content: "";
+  background: linear-gradient(
+    45deg,
+    #00ff00,
+    #00ff00,
+    #00ff00,
+    #00ff00,
+    #00ff00,
+    #00ff00,
+    #00ff00,
+    #00ff00,
+    #00ff00
+  );
+  position: absolute;
+  top: -2px;
+  left: -2px;
+  background-size: 400%;
+  z-index: -1;
+  filter: blur(5px);
+  -webkit-filter: blur(5px);
+  width: calc(100% + 4px);
+  height: calc(100% + 4px);
+  animation: glowing-button-85 20s linear infinite;
+  transition: opacity 0.3s ease-in-out;
+  border-radius: 10px;
+}
+
+@keyframes glowing-button-85 {
+  0% {
+    background-position: 0 0;
+  }
+  50% {
+    background-position: 400% 0;
+  }
+  100% {
+    background-position: 0 0;
+  }
+}
+
+#submit-btn:after {
+  z-index: -1;
+  content: "";
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: #222;
+  left: 0;
+  top: 0;
+  border-radius: 10px;
+}
+.example-col{
+  padding-top: 2%;
+}
+.contact-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color:  #00331A;
+  padding: 3%;
+  color: black;
+  margin: auto;
+  margin-top: 2%;
+}
+input,
+textarea {
+  height: 5vh;
+  width: 100%;
+  margin: 20px;
+  border-top: none;
+  border-right: none;
+  border-left: none;
+  outline: 2px solid rgb(133, 133, 133);
+  
+  
+}
+input:focus {
+    background-color: rgb(241, 240, 240);
+}
+textarea {
+  height: 250px;
+  border-bottom: 2px solid rgb(133, 133, 133);
+}
+textarea:focus {
+    background-color: rgb(241, 240, 240);
 }
 
 </style>
