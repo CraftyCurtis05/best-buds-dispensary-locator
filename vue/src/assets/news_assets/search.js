f2761dbf595bf4b890d9b38ec0041776bbd7953d
CraftyCurtis05 <craftycurtis05@gmail.com>
2024-08-28 19:09:14 -0400

    News search progress

---
+++ vue/src/assets/news_assets/search.js
@@ -0,0 +1,42 @@
+export default [
+    {
+        id: 1,
+        search: "cannabis"
+    },
+    {
+        id: 2,
+        search: "cbd"
+    },
+    {
+        id: 3,
+        search: "marijuana"
+    },
+    {
+        id: 4,
+        search: "medical marijuana"
+    },
+    {
+        id: 5,
+        search: "dispensary"
+    },
+    {
+        id: 6,
+        search: "weed"
+    },
+    {
+        id: 7,
+        search: "edibles"
+    },
+    {
+        id: 8,
+        search: "sativa"
+    },
+    {
+        id: 9,
+        search: "indica"
+    },
+    {
+        id: 10,
+        search: "hemp"
+    }
+]
